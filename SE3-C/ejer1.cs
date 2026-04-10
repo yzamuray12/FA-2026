3eac1684ee331b002c99ebc36986aeb538ee7a04
Adriano <yzamuray12@gmail.com>
2026-04-10 10:22:41 -0500

    subiendo ejer1 en c#

---
+++ SE3-C/ejer1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE3_C
+{
+    internal class ejer1
+    {
+        string nombre, carrera;
+
+        Console.Write("ingrese nombre: ");
+            nombre = Console.readline
+        Console.Write("ingrese carrera: ");
+        carrera = Console.readline
+        console.readkey();
+
+    }
+}
